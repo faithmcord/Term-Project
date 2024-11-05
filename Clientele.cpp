56c14e014385f91a025a880cce0eda671bf7da1b
aPalermooo <ajp2s@login.missouristate.edu>
2024-11-04 20:07:06 -0600

    Header Inventory/Clientele

--- Clientele.cpp
+++ Clientele.cpp
@@ -1,5 +1,18 @@
-//
-// Created by Xander Palermo on 11/4/24.
-//
+/**********************
+*
+* File Name:    Clientele.cpp
+* Author(s):
+* Description:
+*
+* Course:      CSC 455 - Software Quality Assurance
+* Instructor:  Mohammed Belkhouche
+* Project:     Term Project
+* Date:        8 November 2024
+*
+***********************/
 
+#include "Includes.cpp"
 
+class Clientele {
+  public:
+}
--- Inventory.cpp
+++ Inventory.cpp
@@ -1,5 +1,24 @@
-//
-// Created by Xander Palermo on 11/4/24.
-//
+/**********************
+*
+* File Name:    Inventory.cpp
+* Author(s):
+* Description:
+*
+* Course:      CSC 455 - Software Quality Assurance
+* Instructor:  Mohammed Belkhouche
+* Project:     Term Project
+* Date:        8 November 2024
+*
+***********************/
+
+#include "Includes.cpp"
+
+class Inventory{
+  private:
+
+  public:
 
 
+
+
+}
