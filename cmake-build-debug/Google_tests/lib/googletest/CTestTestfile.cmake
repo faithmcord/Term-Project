588b19864987797db06823991b37aa95857b80da
aPalermooo <ajp2s@login.missouristate.edu>
2024-11-13 15:29:21 -0600

    Include cMake and gTest Libraries

---
+++ cmake-build-debug/Google_tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/cinnamon/Documents/Class/CSC455-Quality/Term-Project/google_tests/lib/googletest
+# Build directory: /Users/cinnamon/Documents/Class/CSC455-Quality/Term-Project/cmake-build-debug/google_tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
